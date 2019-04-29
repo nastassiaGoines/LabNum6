1b8344ce9d403a2e61945b6a70a3a2f649948499
nastassiaGoines <stassian@gmail.com>
2019-04-29 10:09:43 -0400

    second commit

--- src/LabNumber6.java
+++ src/LabNumber6.java
@@ -1,4 +1,27 @@
+import java.util.Random;
 
-public class LabNumber6 {
+public class LabNum_6 {
 
+	public static void main(String[] args) {
+		int nSides;
+		int number;
+		int die1;
+		int die2;
+
+		System.out.println("How many sides does each die have? ");
+		System.out.println(nSides);
+		System.out.println("Press '1' to roll");
+		int die1 = generateRandomDieRoll(nSides);
+		int die2 = generateRandomDieRoll(nSides);
+
+	}
+
+	public static int generateRandomDieRoll(int nSides) {
+		int result;
+
+		Random rand = new Random();
+		result = rand.nextInt();
+
+		return result;
+	}
 }
